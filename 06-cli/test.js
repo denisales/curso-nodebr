b9c3841c0635d17f047fec46d97188e0784f4469
Denis Sales <denisales@hotmail.com>
2019-08-12 15:01:19 -0300

    06

---
+++ 06-cli/test.js
@@ -0,0 +1,16 @@
+const {
+    deepEqual,
+    ok
+} = require('assert')
+
+const DEFAULT_ITEM_CADASTRADO = { nome: 'Flash', poder: 'Speed', id: 1 }
+
+
+describe('Suite de manibulação de herois', () => {
+
+    // it('deve cadastrar um heroi, usando arquivos', async () => {
+    //     const expected = DEFAULT_ITEM_CADASTRADO
+    //     ok(null, expected)
+    // })
+
+})
